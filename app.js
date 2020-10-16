782c7ce8ab681ede37688099156e3d7653054ad4
Vlad-B <bogdanionutvlad@outlook.com>
2020-10-16 22:43:35 +0100

    fix express-sslify, add .htaccess

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const http = require('http');
 const enforce = require('express-sslify');
 const app     = express();
 
 app.set('view engine', 'ejs');
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static(__dirname + '/public'));
-
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 app.get('/', (req, res) => {
 	res.render('home');
@@ -20,11 +18,7 @@ app.get('/contact', (req, res) => {
 	res.render('contact');
 });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-// 	console.log(`App is running on port ${ PORT }`);
-// });
-
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Express server is running on port ${ PORT }`);
 });
