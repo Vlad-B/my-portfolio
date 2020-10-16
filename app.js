d5ef4b13b7e845d5f555396227769c29477127f8
Vlad-B <bogdanionutvlad@outlook.com>
2020-10-16 22:30:20 +0100

    add npm express-sslify

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const http = require('http');
+const enforce = require('express-sslify');
 const app     = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 app.get('/', (req, res) => {
 	res.render('home');
@@ -16,7 +20,11 @@ app.get('/contact', (req, res) => {
 	res.render('contact');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`App is running on port ${ PORT }`);
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+// 	console.log(`App is running on port ${ PORT }`);
+// });
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
